7541bcfe024cd20f804583b8312980767520a83b
Victor <82475961+Vitoboos@users.noreply.github.com>
2023-11-07 17:45:23 -0400

    V2
    Made some adjustments to fields, added import-export.

--- database/models.py
+++ database/models.py
@@ -25,6 +25,7 @@ class Empleado(models.Model):
     nombre = models.CharField(max_length=50, default='No asignado')
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     cargo = models.CharField(choices=CARGOS, max_length=50, default='Sin cargo')
+    cedula = models.IntegerField(default=0)
     
     def __str__(self):
         return self.nombre
@@ -36,7 +37,7 @@ class Equipo(models.Model):
     # Datos corporativos
     
     usuario = models.ForeignKey(Empleado, on_delete=models.CASCADE)
-    
+    bien_nacional = models.CharField(max_length=6, default="000000")
     # Datos de fabrica
 
     marca = models.CharField(max_length=50, default='No asignado')
@@ -53,8 +54,7 @@ class Equipo(models.Model):
     mac = models.CharField(max_length=50, default='No asignado')
 
     def __str__(self):
-        return self.usuario
-    
+        return self.usuario.nombre
 # Telefonos celulares    
 
 class Telefono(models.Model):   
@@ -63,7 +63,8 @@ class Telefono(models.Model):
     
     usuario = models.ForeignKey(Empleado, on_delete=models.CASCADE)
     numero = models.CharField(max_length=50, default='No asignado')
-    
+    bien_nacional = models.CharField(max_length=6, default="000000")
+
     # Datos de fabrica
 
     marca = models.CharField(max_length=50, default='No asignado')
@@ -75,13 +76,14 @@ class Telefono(models.Model):
     mac = models.CharField(max_length=50, default='No asignado')
 
     def __str__(self):
-        return self.usuario
+        return self.usuario.nombre
     
 class Impresora(models.Model):
 
     # Datos corporativos
     
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    bien_nacional = models.CharField(max_length=6, default="000000")
 
     # Datos de fabrica
     
@@ -93,10 +95,14 @@ class Impresora(models.Model):
     ipv4 = models.CharField(max_length=50, default='No asignado')
     mac = models.CharField(max_length=50, default='No asignado')
     def __str__(self):
-        return self.ipv4 + ' ' + self.departamento
+        return self.ipv4 + ' ' + self.departamento.nombre
     
     
 class Switch(models.Model):
+
+    # Datos corporativos
+    
+    bien_nacional = models.CharField(max_length=6, default="000000")
 
     # Datos de fabrica
 
@@ -112,6 +118,11 @@ class Switch(models.Model):
         return self.ipv4
 
 class Router(models.Model):
+    
+    # Datos corporativos
+    
+    bien_nacional = models.CharField(max_length=6, default="000000")
+    
     # Datos de fabrica
 
     marca = models.CharField(max_length=50, default='No asignado')
@@ -126,6 +137,11 @@ class Router(models.Model):
         return self.ipv4
     
 class Desincorporacion(models.Model):
+    
+    # Datos de usuario
+    
+    bien_nacional = models.CharField(max_length=6, default="000000")
+    
     # Datos corporativos
 
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE, default=0)
@@ -138,7 +154,7 @@ class Desincorporacion(models.Model):
     modelo = models.CharField(max_length=50, default='No asignado')
     
     def __str__(self):
-        return self.usuario
+        return self.departamento.nombre + ' ' + self.modelo
 
 
     
